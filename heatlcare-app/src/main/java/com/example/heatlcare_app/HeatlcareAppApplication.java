cdb42843286943121208a672b7f56f35714fc2ab
Cao Quang Phong <0306191152@caothang.edu.vn>
2025-05-31 00:31:19 +0700

    first commit

---
+++ heatlcare-app/src/main/java/com/example/heatlcare_app/HeatlcareAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.heatlcare_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatlcareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatlcareAppApplication.class, args);
+	}
+
+}
